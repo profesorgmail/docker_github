5711d7ac5d83bd2625d57415afcef6d4718a0ece
profesorgmail <profesorfemete@gmail.com>
2022-05-18 19:00:05 +0100

    corregimos errores en app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-import os
+import os, time
 
-os.system("mkdir vvv")
 print("hecho")
+
+a = int(input("dame: "))
+b = a**3
+print("esperamos 5 seg.")
+for i in range(5):
+    time.sleep(1)
+    print(i+1)
+print("Termine, {}".format(b))
